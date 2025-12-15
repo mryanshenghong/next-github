2be9bd38f1bd4097aed4fb56f5fc131cde23084a
mryanshenghong <mryanshenghong@gmail.com>
2025-12-15 08:05:32 -0500

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-    apps:[
-        {
-            name: 'next-project',
-            script:'./server.js',
-            instances:1,
-            autorestart:true,
-            watch:false,
-            max_memory_restart: '1G',
-            env:{
-                NODE_ENV:'production',
-            }
-        }
-    ]
-}
+  apps: [
+    {
+      name: "next-project",
+      script: "./server.js",
+      instances: 1,
+      log_file: "/mnt/volume2/pm2/logs/next_github.log",
+      error_file: "/mnt/volume2/pm2/logs/next_github.log",
+      autorestart: false,
+      watch: false,
+      max_memory_restart: "500M",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
